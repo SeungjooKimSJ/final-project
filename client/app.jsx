73e48328ac25444065731eddb02e0386aef20410
Seungjoo Kim <sjskybone@gmail.com>
2021-05-25 18:14:44 -0700

    Working on the server/index.js to store transaction data. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import parseRoute from './lib/parse-route';
 import Home from './pages/home';
 
 export default class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const locationChanges = parseRoute(window.location.hash);
+
+      this.setState({
+        route: locationChanges
+      });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+
+    if (route.path === '') {
+      return <Transaction />;
+    }
     return <Home />;
+  }
+
+  render() {
+    return (
+      <>
+        <Home />
+        {this.renderPage()}
+      </>
+    );
   }
 }
---
+++ client/components/calculator.jsx
@@ -0,0 +1,3 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import toDollars from '../lib/to-dollars';
